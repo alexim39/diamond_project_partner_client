66dfbb553fa551366051bb10b1f2eeac1cd7d10e
Alex Imenwo <aleximenwo@gmail.com>
2024-07-19 17:42:13 +0100

    completed the facebook ad campaign page

---
+++ src/app/dashboard/partner/create-campaign/create-campaign.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+export interface CreateCampaignInterface {
+   /*  ageRangeTarget:  string;
+      genderTarget:  string;
+      locationTarget:  string;
+      educationTarget: string;
+      relationshipTarget:  string;
+      adObjective:  string;
+      successMeasurement:  string;
+      budgetType:  string;
+      budgetAmount: number;
+      campaignStartDate:  string;
+      campaignEndDate: string;
+      noEndDate: boolean;
+      adFormat:  string;
+      deviceType: string;
+      FacebookFeed: boolean
+      InstagramFeed: boolean
+      InstagramStories:boolean
+      FacebookStories: boolean
+      AudienceNetwork: boolean
+      MessengerInbox: boolean; */
+      targetAudience: any;
+      marketingObjectives: any;
+      budget: any;
+      adDuration: any;
+      adFormat: any;
+}
+  
+
+@Injectable()
+export class CreateCampaignService {
+  // Define API
+  //api = 'https://diamondprojectapi-y6u04o8b.b4a.run/';
+  api = 'http://localhost:3000';
+  constructor(private http: HttpClient) {}
+  /*========================================
+    CRUD Methods for consuming RESTful API
+  =========================================*/
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  // Error handling
+  private handleError(error: any) {
+    let errorMessage: {code: string, message: string};
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = {'code': error.status, 'message': error.message};
+    }
+    //window.alert(errorMessage);
+    return throwError(() => {
+      return errorMessage;
+    });
+  }
+
+
+  // facebook
+  facebook(facebookData: CreateCampaignInterface): Observable<CreateCampaignInterface> {
+    console.log('form record', facebookData);
+    return this.http
+      .post<CreateCampaignInterface>(this.api + '/campaign/facebook', facebookData, { withCredentials: true })
+      .pipe(retry(1), catchError(this.handleError));
+  }
+
+   
+}
