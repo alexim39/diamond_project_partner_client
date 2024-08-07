0355ae362ddf2799af8a0fdd1a05100562f5aa59
Alex Imenwo <aleximenwo@gmail.com>
2024-08-07 23:27:35 +0100

    added linkedin campaign

--- src/app/dashboard/partner/dashboard.component.ts
+++ src/app/dashboard/partner/dashboard.component.ts
@@ -20,13 +20,12 @@ import { MarketingChannelsComponent } from './create-campaign/marketing-channels
 import { PartnerInterface, PartnerService } from '../../_common/services/partner.service';
 import { Emitters } from '../../_common/emitters/emitters';
 
-// Define the SubmenuKey type
 type SubmenuKey = 'tools' | 'community' | 'analytics' | 'settings' | 'activities' | 'mentorship';
 
 @Component({
   selector: 'async-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss',
+  styleUrls: ['./dashboard.component.scss'],
   standalone: true,
   providers: [PartnerService, PartnerAuthService],
   imports: [
@@ -60,8 +59,7 @@ export class DashboardComponent implements OnDestroy {
   isTablet!: boolean;
   isDesktop!: boolean;
 
-  isLoading: boolean = false; // Flag for loading state
-  
+  isLoading: boolean = false;
 
   submenus: Record<SubmenuKey, boolean> = {
     tools: false,
@@ -72,20 +70,21 @@ export class DashboardComponent implements OnDestroy {
     mentorship: false,
   };
 
+  subSubmenus: Record<string, boolean> = {};
+
   partner!: PartnerInterface;
-  
 
   constructor(
-    private deviceService: DeviceDetectorService, 
+    private deviceService: DeviceDetectorService,
     private router: Router,
     private partnerAuthService: PartnerAuthService,
     private partnerService: PartnerService,
   ) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
-        this.isLoading = true; // Set loading to true on navigation start
+        this.isLoading = true;
       } else if (event instanceof NavigationEnd) {
-        this.isLoading = false; // Set loading to false on navigation end
+        this.isLoading = false;
       }
     });
   }
@@ -95,25 +94,19 @@ export class DashboardComponent implements OnDestroy {
     this.isTablet = this.deviceService.isTablet();
     this.isDesktop = this.deviceService.isDesktop();
 
-    // get current signed in user
     this.subscriptions.push(
       this.partnerService.getPartner().subscribe(
         res => {
-          this.partner = res as PartnerInterface
+          this.partner = res as PartnerInterface;
           Emitters.authEmitter.emit(true);
-          //console.log(this.partner)
-          
-          // Update the partner object sharing service
           this.partnerService.updatePartnerService(this.partner);
         },
         error => {
-          //console.log(error)
           Emitters.authEmitter.emit(false);
-          // redirect to home page
-          this.router.navigate(['/'])
+          this.router.navigate(['/']);
         }
       )
-    )
+    );
   }
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -122,39 +115,44 @@ export class DashboardComponent implements OnDestroy {
       shareReplay()
     );
 
-  // scroll to top when clicked
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
   signOut(): void {
-    //this.loadingSpinnerService.show();
-
     this.subscriptions.push(
       this.partnerAuthService.signOut({}).subscribe(res => {
-        //this.authenticated = false;
-        //this.loadingSpinnerService.hide()
-        // redirect to login page
-        this.router.navigate(['/'])
+        this.router.navigate(['/']);
       })
-    )
+    );
 
     this.scrollToTop();
   }
 
-
   ngOnDestroy() {
-    // unsubscribe list
     this.subscriptions.forEach(subscription => {
       subscription.unsubscribe();
     });
   }
 
   toggleSubmenu(menu: SubmenuKey) {
+    for (let key in this.submenus) {
+      if (key !== menu) {
+        this.submenus[key as SubmenuKey] = false;
+      }
+    }
     this.submenus[menu] = !this.submenus[menu];
   }
 
   isSubmenuOpen(menu: SubmenuKey): boolean {
     return this.submenus[menu];
+  }
+
+  toggleSubSubmenu(submenu: string) {
+    this.subSubmenus[submenu] = !this.subSubmenus[submenu];
+  }
+
+  isSubSubmenuOpen(submenu: string): boolean {
+    return this.subSubmenus[submenu];
   }
 }
