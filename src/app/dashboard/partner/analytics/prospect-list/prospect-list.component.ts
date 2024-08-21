ab6d9e04a9459c177091d9115ddb80ac57abc946
Alex Imenwo <aleximenwo@gmail.com>
2024-08-21 19:46:24 +0100

    updated the days ago on prospect table

--- src/app/dashboard/partner/analytics/prospect-list/prospect-list.component.ts
+++ src/app/dashboard/partner/analytics/prospect-list/prospect-list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, Input, OnDestroy, OnInit} from '@angular/core';
+import { AfterViewInit, Component, inject, Input, OnChanges, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import { PartnerInterface } from '../../../../_common/services/partner.service';
 import { MatIconModule } from '@angular/material/icon';
 import { HelpDialogComponent } from '../../../../_common/help-dialog.component';
@@ -12,11 +12,13 @@ import { Subscription } from 'rxjs';
 import { MatInputModule } from '@angular/material/input';  
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatTableModule } from '@angular/material/table';
 import { Router, RouterModule } from '@angular/router';
-import { ProspectListFilterPipe } from './prospect-list-filter.pipe';
 import { AnalyticsService, ProspectListInterface } from '../analytics.service';
 import { ProspectResponseComponent } from './prospect-response.component';
+import { timeAgo } from '../../../../_common/date-util';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+
 
 /**
  * @title Contacts
@@ -27,41 +29,63 @@ import { ProspectResponseComponent } from './prospect-response.component';
   styleUrls: ['prospect-list.component.scss'],
   standalone: true,
   providers: [AnalyticsService],
-  imports: [CommonModule, MatIconModule, RouterModule, MatTableModule, MatIconModule, MatFormFieldModule, MatProgressBarModule, ProspectListFilterPipe, MatButtonModule, FormsModule,MatInputModule,MatSelectModule],
+  imports: [CommonModule, MatIconModule, RouterModule, MatTableModule, MatIconModule, MatPaginatorModule, MatFormFieldModule, MatProgressBarModule, MatButtonModule, FormsModule,MatInputModule,MatSelectModule],
 })
-export class ProspectListComponent implements OnInit, OnDestroy {
+export class ProspectListComponent implements OnInit, OnDestroy  {
     @Input() partner!: PartnerInterface;
     readonly dialog = inject(MatDialog);
     @Input() prospectList!: ProspectListInterface;
 
     subscriptions: Array<Subscription> = [];
 
-    dataSource: Array<any> = [];
+    dataSource = new MatTableDataSource<any>([]);  
     isEmptyRecord = false;
   
     filterText: string = '';
   
-    displayedColumns: string[] = ['name', 'phone', 'email',  'date', 'detail', 'action'];
+    displayedColumns: string[] = ['name', 'phone', 'email',  'date', 'dateAgo', 'detail', 'action'];
+    timeAgoList: string[] = [];  
+
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
 
     constructor(
       private analyticsService: AnalyticsService,
       private router: Router,
     ) {}
 
-
+ 
     ngOnInit(): void {
         if (this.prospectList.data) {  
         //console.log(this.prospectContact.data)
-        this.dataSource = this.prospectList.data.sort((a, b) => {  
+        this.dataSource.data  = this.prospectList.data.sort((a, b) => {  
           // Use the getTime() method to compare the Date values  
           return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();  
         });  
     
-        if (this.dataSource.length === 0) {  
+        if (this.dataSource.data.length === 0) {  
           this.isEmptyRecord = true;  
         }  
       }  
+      
+      // Custom filter predicate to filter by name
+    this.dataSource.filterPredicate = (data: any, filter: string) => {
+      return data.name.toLowerCase().includes(filter.toLowerCase()) || data.surname.toLowerCase().includes(filter.toLowerCase());
+    };
+
     }
+
+    applyFilter(filterValue: string) {
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
+
+    ngAfterViewInit() {
+      this.dataSource.paginator = this.paginator;
+    }
+  
+
+    getDateAgo(element: any): string {  
+      return timeAgo(new Date(element.updatedAt)); // Assuming element.updatedAt holds the date  
+    } 
 
 
     ViewResponse(prospect: ProspectListInterface) {
